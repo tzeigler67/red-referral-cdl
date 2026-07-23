9276f72aaf96552e9aef8f2fefd7b3d39962a8ee
tzeigler67 <tzeigler6@gmail.com>
2026-07-22 23:18:07 -0500

    Build 1

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// placeholder
+document.getElementById('referralForm').addEventListener('submit',function(e){
+e.preventDefault();
+alert('Build 1 placeholder: form submission will be connected in a later build.');
+});
